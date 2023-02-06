1d2b07f31e2058f201100e308e0a1d6c81cfcdae
BeeHive CyberSecurity <102701375+BeeHiveCyberSecurity@users.noreply.github.com>
2023-02-06 06:24:16 -0700

    Update health-check.sh

--- health-check.sh
+++ health-check.sh
@@ -57,6 +57,6 @@ then
   git config --global user.name 'BeeHive CyberSecurity'
   git config --global user.email 'security@beehive.systems'
   git add -A --force logs/
-  git commit -am '[Automated] Update Logs'
+  git commit -o -am '[Automated] Update Logs'
   git push
 fi
